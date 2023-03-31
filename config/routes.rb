1db29f68a2bebd6fee578262a34988407d627c5c
Brittney <b.holtstone@gmail.com>
2023-03-31 14:07:26 -0400

    adds styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  #goes to pages action and looks for a home action
    root "pages#home"
 end
