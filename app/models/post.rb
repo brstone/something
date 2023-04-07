1260743b92f4428e60884707c9318e09deeed535
Brittney <b.holtstone@gmail.com>
2023-04-07 15:48:12 -0400

    quit crying and fixed this shit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   #in model, do your db connections
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :body, presence: true, length: { minimum: 5, maximum: 3000 }
+  validates :body, presence: true
   belongs_to :user
   #dependent destroy means that if you delete the post you delete all of the comments
   has_many :comments, dependent: :destroy
